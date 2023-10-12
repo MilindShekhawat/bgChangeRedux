c37df67c3deccddbc94fd2abb1d38ffcdf15a875
MilindShekhawat <milindshekhawat10@gmail.com>
2023-10-12 16:06:58 +0530

    Cleaned and added comments

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+//1.Set page to CSR
+'use client'
+//2. Import Buttons, provider and store
+import Buttons from './components/Buttons'
+import { Provider } from 'react-redux'
+import {store} from '../redux/store'
 
 export default function Home() {
+  //3. Encapsulate the Button component with provider.
   return (
-    <main className={styles.main}>
-      Hello
-    </main>
+    <Provider store = {store}>
+      <Buttons />
+    </Provider>
   )
 }
